abb682a279017cd19b7b5fe3ffdb0373e38807a4
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-13 20:47:11 +0600

    services

---
+++ client/src/pages/home/Services/ServiceCard.jsx
@@ -0,0 +1,17 @@
+const ServiceCard = ({ service }) => {
+    const { title, img, price } = service;
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img className="h-60 w-full object-cover" src={img} alt="" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{title}!</h2>
+                <p>Price: ${price}</p>
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServiceCard;
---
+++ client/src/pages/home/Services/Services.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(response => response.json())
+            .then(data => setServices(data))
+    }, [])
+    return (
+        <div>
+            <div className='text-center'>
+                <h3 className='text-2xl font-bold text-orange-500'>Our Services</h3>
+                <h2 className='text-4xl font-bold'>Our Service Area</h2>
+                <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ducimus corrupti dolores! Rem dicta atque corrupti sit accusantium et veniam iure harum ad soluta nisi ratione, sequi officiis natus mollitia provident molestiae aliquam est! Repellat deleniti repudiandae blanditiis maxime quod sed rerum enim est, dicta laborum deserunt quae corporis exercitationem.</p>
+            </div>
+
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-10'>
+
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
