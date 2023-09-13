bce1ad5fbabe2d0f2d5a609ff5e0c17797354e39
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-13 14:37:50 +0600

    folder structer, navbar, and footer

---
+++ client/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import router from './routes/Routes.jsx';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  </div>
+)
