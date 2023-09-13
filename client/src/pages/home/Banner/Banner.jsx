846cc86e981ba804cd26d3f20706ce3be17dada1
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-13 15:35:16 +0600

    banner data

--- client/src/pages/home/Banner/Banner.jsx
+++ client/src/pages/home/Banner/Banner.jsx
@@ -12,7 +12,20 @@ const Banner = () => {
     return (
         <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src={img1} className="w-full" />
+                <img src={img1} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center h-full pl-8  left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+                    <div className='text-white space-y-7 w-1/2'>
+                        <h2 className='text-6xl  font-bold'>Affordable Price For Car Servicing</h2>
+                        <p className='text-xl'>There are many variations of passages of available, but the majority have suffered alteration in some forms</p>
+                        <div>
+                            <button className="btn btn-error mr-5">Discover More</button>
+                            <button className="btn btn-outline btn-error">Latest Projects</button>
+                        </div>
+
+                    </div>
+                </div>
+
                 <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-5">
                     <a href="#slide4" className="btn btn-circle mr-5">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
