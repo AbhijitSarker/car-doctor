580d5aefab92bbf2c7de9881069ff34749a56295
AbhijitSarker <abhijitsarker03@gmail.com>
2023-09-14 16:03:50 +0600

    service checkout

---
+++ client/src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Checkout = () => {
+    const service = useLoaderData()
+    const { title, _id } = service;
+    console.log(service);
+    return (
+        <div className="card-body">
+            <form>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
+        </div>
+
+    );
+};
+
+export default Checkout;
--- client/src/pages/home/Services/ServiceCard.jsx
+++ client/src/pages/home/Services/ServiceCard.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
+
 const ServiceCard = ({ service }) => {
-    const { title, img, price } = service;
+    const { title, img, price, _id } = service;
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img className="h-60 w-full object-cover" src={img} alt="" /></figure>
@@ -7,7 +10,7 @@ const ServiceCard = ({ service }) => {
                 <h2 className="card-title">{title}!</h2>
                 <p>Price: ${price}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn bg-orange-600 text-white  hover:bg-orange-500 hover:text-white">Buy Now</button>
+                    <Link to={`/checkout/${_id}`}><button className="btn bg-orange-600 text-white  hover:bg-orange-500 hover:text-white">Book Now</button></Link>
                 </div>
             </div>
         </div>
